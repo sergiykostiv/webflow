2397f2e293d2b32d84984ef073b69c81564ec282
Sergiy Kostiv <sergiy.kostiv@gmail.com>
2024-01-07 21:39:49 +0200

    some fix

--- js/main.js
+++ js/main.js
@@ -107,37 +107,3 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
 		}
 	});
 });
-
-// const animItems = document.querySelectorAll('.anim-items');
-
-// if (animItems.lenght > 0) {
-//   window.addEventListener('scroll', animOnScroll)
-//   function animOnScroll() {
-//     for (let index = 0; index < animItems.length; index++) {
-//       const animItem = animItems[index];
-//       const animItemHeight = animItem.offsetHeight;
-//       const animItemOffset = offset(animItem).top;
-//       const animStart = 4;
-
-//       let animItemPoint = window.innerHeight - animItemHeight / animStart;
-
-//       if (animItemPoint > window.innerHeight) {
-//         animItemPoint = window.innerHeight - window.innerHeight / animStart;
-//       }
-
-//       if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-//         animItem.classList.add('.active');
-//         console.log('active')
-//       } else {
-//         animItem.classList.remove('.active');
-//       }
-//     }
-//   }
-
-//   function offset(el) {
-//     const rect = el.getBoundingClientRect(),
-//       scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-//       scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-//   }
-// }
